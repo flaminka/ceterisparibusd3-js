4267c2f78d5d13b50f85d75e9f68c9f1b99c0296
Ewa Baranowska <ewa.baranowska4@wp.pl>
2018-10-02 20:02:54 +0200

    Initial test commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ceterisparibusd3-js
 JS library for interactive, D3 based plots for XAI method implemented in R package called ceterisParibus.
+
+Local repository - private tips
+Edit files in given folder in Interactive XAI for DALEX, but then copy it to github folder in documents and push to github repo in github.com.
---
+++ src/index.js
@@ -0,0 +1,20 @@
+// jesli chcialabym tu zaladowac external file to tak
+/*
+var starWarsNames = require('./starwars-names.json');
+var uniqueRandomArray =require('unique-random-array');
+var d3 = require('D3'); - by zaladowac pakiet??
+raczej import {path} from "d3-path"; 
+czyli import {select} from "d3"; w pliku z dana funkcja
+a potem juz normalnie uzywac
+patrz np plik https://github.com/d3/d3-shape/blob/acb6053eae36fe6dcdb0b62fe2691bee2acc511b/src/line.js
+*/
+
+
+module.exports = {
+// chyba nazwy funkcji, ktore beda eksportowane
+/* oni dali to tak
+    all: starWarsNames,  // przypisal ten obiekt powyzej do property nazwanej all
+    random: uniqueRandomArray(starWarsNames)
+*/
+
+};  
